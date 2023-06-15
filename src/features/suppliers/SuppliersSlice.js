2fc0d08dca3e7991ccb41ce82ef7757b27395e69
josesoto <josesoto@itop.es>
2023-06-15 20:17:07 +0100

    feat(supplier): updated slice

--- src/features/suppliers/SuppliersSlice.js
+++ src/features/suppliers/SuppliersSlice.js
@@ -1,113 +1,113 @@
-import axios from "axios";
-import { baseUrl } from "../baseApi";
-import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from 'axios'
+import { baseUrl } from '../baseApi'
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
 
+const suppliersApi = axios.create({ baseURL: `${baseUrl}/supplier` })
+const suppliersAdminApi = axios.create({ baseURL: `${baseUrl}/admin/supplier` })
 
-const suppliersApi = axios.create({baseURL:`${baseUrl}/supplier`})
-const suppliersAdminApi = axios.create({baseURL:`${baseUrl}/admin/supplier`})
+export const fetchAllsuppliers = createAsyncThunk(
+    'suppliers/fetchAll',
+    async (_, { rejectWithValue }) => {
+        try {
+            const token = localStorage.getItem('at')
+            const { data } = await suppliersApi.get(`/all`, {
+                headers: { Authorization: `Bearer ${token}` },
+            })
+            return data
+        } catch (error) {
+            return rejectWithValue(error.message)
+        }
+    },
+)
 
-export const fetchAllsuppliers = createAsyncThunk('suppliers/fetchAll', async (_,{rejectWithValue})=>{
-    try {
-        const token = localStorage.getItem('at')
-        const {data} = await suppliersApi.get(`/all`,{
-            headers:{Authorization:`Bearer ${token}`,
-        }})
-        return data
-    } catch (error) {
-        return rejectWithValue(error.message)
-    }
-})
+export const createSupplier = createAsyncThunk(
+    'suppliers/create',
+    async (payload, { rejectWithValue }) => {
+        try {
+            const token = localStorage.getItem('at')
+            const { data } = await suppliersAdminApi.post(`/create`, payload, {
+                headers: { Authorization: `Bearer ${token}` },
+            })
+            return data
+        } catch (error) {
+            return rejectWithValue(error.message)
+        }
+    },
+)
 
-export const createSupplier = createAsyncThunk('suppliers/create', async (payload, {rejectWithValue})=>{
-    try {
-        const token = localStorage.getItem('at')
-        const {data} = await suppliersAdminApi.get(`/create`,
-        payload
-        ,{
-            headers:{Authorization:`Bearer ${token}`,
-        }})
-        return data
-    } catch (error) {
-        return rejectWithValue(error.message)
-    }
-})
-
-export const updateSupplier = createAsyncThunk('suppliers/update', async (payload, {rejectWithValue})=>{
-    try {
-        const token = localStorage.getItem('at')
-        const {data} = await suppliersAdminApi.get(`/update`,
-        payload
-        ,{
-            headers:{Authorization:`Bearer ${token}`,
-        }})
-        return data
-    } catch (error) {
-        return rejectWithValue(error.message)
-    }
-})
-
-
+export const updateSupplier = createAsyncThunk(
+    'suppliers/update',
+    async (payload, { rejectWithValue }) => {
+        try {
+            const token = localStorage.getItem('at')
+            const { data } = await suppliersAdminApi.put(`/update`, payload, {
+                headers: { Authorization: `Bearer ${token}` },
+            })
+            return data
+        } catch (error) {
+            return rejectWithValue(error.message)
+        }
+    },
+)
 
 const initialState = {
-    suppliers:[],
+    suppliers: [],
     status: 'idle',
-    error: null
+    error: null,
 }
 
-
 const supplierSlice = createSlice({
-    name:'suppliers',
+    name: 'suppliers',
     initialState,
-    extraReducers(builder){
+    extraReducers(builder) {
         builder
-        .addCase(fetchAllsuppliers.pending, (state)=>{
-            state.status = 'loading'
-        })
+            .addCase(fetchAllsuppliers.pending, (state) => {
+                state.status = 'loading'
+            })
 
-        .addCase(fetchAllsuppliers.fulfilled, (state,action)=>{
-            state.status = 'succeded'
-            const payload = action.payload
-            state.suppliers = payload
-        })
+            .addCase(fetchAllsuppliers.fulfilled, (state, action) => {
+                state.status = 'succeded'
+                const payload = action.payload
+                state.suppliers = payload
+            })
 
-        .addCase(fetchAllsuppliers.rejected, (state, action)=>{
-            state.status = 'rejected'
-            state.error = action.error.message
-        })
-        .addCase(createSupplier.pending, (state)=>{
-            state.status = 'loading'
-        })
+            .addCase(fetchAllsuppliers.rejected, (state, action) => {
+                state.status = 'rejected'
+                state.error = action.error.message
+            })
+            .addCase(createSupplier.pending, (state) => {
+                state.status = 'loading'
+            })
 
-        .addCase(createSupplier.fulfilled, (state,action)=>{
-            state.status = 'succeded'
-            const payload = action.payload
-            state.suppliers.push(payload)
-        })
+            .addCase(createSupplier.fulfilled, (state) => {
+                state.status = 'succeded'
+                state.status = 'idle'
+            })
 
-        .addCase(createSupplier.rejected, (state, action)=>{
-            state.status = 'rejected'
-            state.error = action.error.message
-        })
+            .addCase(createSupplier.rejected, (state, action) => {
+                state.status = 'rejected'
+                state.error = action.error.message
+            })
 
-        .addCase(updateSupplier.pending, (state)=>{
-            state.status = 'loading'
-        })
+            .addCase(updateSupplier.pending, (state) => {
+                state.status = 'loading'
+            })
 
-        .addCase(updateSupplier.fulfilled, (state,)=>{
-            state.status = 'succeded'
-            state.status = 'idle'
-        })
+            .addCase(updateSupplier.fulfilled, (state) => {
+                state.status = 'succeded'
+                state.status = 'idle'
+            })
 
-        .addCase(updateSupplier.rejected, (state, action)=>{
-            state.status = 'rejected'
-            state.error = action.error.message
-        })
-    }
+            .addCase(updateSupplier.rejected, (state, action) => {
+                state.status = 'rejected'
+                state.error = action.error.message
+            })
+    },
 })
 
-
 export const selectAllSuppliers = (state) => state.suppliers?.suppliers
-export const selectsuppliersStatus = (state)=> state.suppliers.status
-export const selectsuppliersError = (state)=> state.suppliers.error
-export const selectOneSupplierById = (state, id) => state.suppliers.suppliers.find((item)=> item.item_id === id)
+export const selectsuppliersStatus = (state) => state.suppliers.status
+export const selectsuppliersError = (state) => state.suppliers.error
+export const selectOneSupplierById = (state, id) =>
+    state.suppliers.suppliers.find((item) => item.item_id === id)
 export default supplierSlice.reducer
